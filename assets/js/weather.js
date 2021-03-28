1e911b9f111298bb417dd218f20a4fc195a573cf
Michelle <michey.asmah21@gmail.com>
2021-03-28 18:54:22 -0400

    start weather.js

---
+++ assets/js/weather.js
@@ -0,0 +1,8 @@
+var citySerach = document.querySelector("#city-search");
+var caption = document.querySelector("#caption");
+var weatherInfo = document.querySelector(".all-cards");
+
+var getWeatherInfo = function(city) {
+
+}
+
