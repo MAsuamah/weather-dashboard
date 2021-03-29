3a70c9bd132d5c5862e98f0d6c48b79a7d521c5c
Michelle <michey.asmah21@gmail.com>
2021-03-29 17:03:51 -0400

    uv index colour code

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -6,9 +6,9 @@ var currentIcon = document.querySelector("#current-date-icon");
 var currentTemp = document.querySelector("#current-date-temp");
 var currentHumidity = document.querySelector("#current-date-humidity");
 var currentWind = document.querySelector("#current-date-wind");
+var uvIndex = document.querySelector("#current-date-uv")
 var caption = document.querySelector("#caption");
 var cards = document.querySelector(".all-cards");
-
 
 var getWeatherInfo = function () { 
   fetch("http://api.openweathermap.org/data/2.5/weather?q=" + document.querySelector("#city-search").value + "&appid=dba30060fc955c512265e193bbe9bba7&units=metric").then(function (response) {
@@ -36,22 +36,38 @@ var getWeatherInfo = function () {
   });
 }
 
-var displayCurrentWeather = function(data, search) {
+var displayCurrentWeather = function(present, forecast) {
   while (currentIcon.firstChild) {
     currentIcon.firstChild.remove()
   }
+
   caption.setAttribute("class", "hidden")
   cards.removeAttribute("class", "hidden")
-  cityHeader.textContent = data.name
+
+  cityHeader.textContent = present.name
   currentDate.textContent = moment().format("YYYY-MM-DD");
   var weatherIcon = document.createElement('img');
-  weatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+  weatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + present.weather[0].icon + ".png");
   currentIcon.appendChild(weatherIcon);
-  currentTemp.innerHTML = "Temperature:" + " " + data.main.temp + "&deg;C"
-  currentHumidity.innerHTML = "Humidity:" + " " + data.main.humidity + "&percnt;"
-  currentWind.innerHTML = "Wind Speed:" + " " + data.wind.speed + " " + "km/h"
-  console.log(data);
-  console.log(search);
+  currentTemp.innerHTML = "Temperature:" + " " + present.main.temp + "&deg;C"
+  currentHumidity.innerHTML = "Humidity:" + " " + present.main.humidity + "&percnt;"
+  currentWind.innerHTML = "Wind Speed:" + " " + present.wind.speed + " " + "km/h"
+  uvIndex.textContent = "UV Index:" + " " + forecast.current.uvi
+  
+  if (forecast.current.uvi >= 0 && forecast.current.uvi < 3) {
+    uvIndex.setAttribute("class", "low")
+  } else if (forecast.current.uvi >= 3 && forecast.current.uvi < 6) {
+    uvIndex.setAttribute("class", "moderate")
+  } else if (forecast.current.uvi >= 6 && forecast.current.uvi < 8) {
+    uvIndex.setAttribute("class", "high")
+  } else if (forecast.current.uvi >= 8 && forecast.current.uvi < 11) {
+    uvIndex.setAttribute("class", "very-high")
+  } else if (forecast.current.uvi >= 11) {
+    uvIndex.setAttribute("class", "extreme")
+  }
+
+  console.log(present);
+  console.log(forecast);
 };
 
 var formSubmitHandler = function(event) {
@@ -66,8 +82,6 @@ var formSubmitHandler = function(event) {
     alert("Please enter a city");
   } 
 };
-
-
 
 
 searchForm.addEventListener("submit", formSubmitHandler);
