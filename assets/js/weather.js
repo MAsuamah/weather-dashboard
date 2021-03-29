fc270c3265d33a8e9a492e3ed16d26ceb7754e41
Michelle <michey.asmah21@gmail.com>
2021-03-28 20:07:02 -0400

    set up api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,8 +1,14 @@
-var citySerach = document.querySelector("#city-search");
-var caption = document.querySelector("#caption");
-var weatherInfo = document.querySelector(".all-cards");
-
 var getWeatherInfo = function(city) {
+  // format the github api url
+  var apiUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=dba30060fc955c512265e193bbe9bba7&units=metric";
 
-}
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    console.log(response);
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
 
+getWeatherInfo("toronto");
